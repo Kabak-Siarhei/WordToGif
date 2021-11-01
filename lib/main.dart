199ac8939cff5fb189fdaed4617d13a6b76d9a6d
bagasayn <bagasayn@gmail.com>
2021-11-01 22:08:54 +0300

    Added localization Fixed Switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'l10n/all_locales.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:word_to_gif/home/blocs/theme/theme_bloc.dart';
 import 'package:word_to_gif/home/home.dart';
-
-
 
 void main() {
   runApp(MyApp());
@@ -12,16 +14,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ThemeBloc(),
-      child: BlocBuilder<ThemeBloc, ThemeChangeState>(
-        builder: (context, state) {
+    return MultiProvider(
+      providers: [
+        BlocProvider<ThemeBloc>(
+          create: (context) => ThemeBloc(),
+        )
+      ],
+      child:
+          BlocBuilder<ThemeBloc, ThemeChangeState>(builder: (context, state) {
         bool isDarkTheme = false;
         if (state is ThemeChangeState) {
           isDarkTheme = state.isDarkTheme;
         }
         return MaterialApp(
-          title: 'WordToGif',
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: AllLocales.all,
           theme: isDarkTheme ? ThemeData.dark() : ThemeData.light(),
           home: HomePage(),
         );
