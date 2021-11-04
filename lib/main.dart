f64a08a6626501a2b2525222745c4639b7018e94
bagasayn <bagasayn@gmail.com>
2021-11-04 10:26:20 +0300

    deleted navigation 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
 import 'l10n/all_locales.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:word_to_gif/home/blocs/theme/theme_bloc.dart';
@@ -14,18 +13,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        BlocProvider<ThemeBloc>(
-          create: (context) => ThemeBloc(),
-        )
-      ],
+    return BlocProvider<ThemeBloc>(
+      create: (context) => ThemeBloc(),
       child:
           BlocBuilder<ThemeBloc, ThemeChangeState>(builder: (context, state) {
         bool isDarkTheme = false;
+
         if (state is ThemeChangeState) {
           isDarkTheme = state.isDarkTheme;
         }
+
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           localizationsDelegates: [
